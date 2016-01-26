9935a57965ef55a4e62a96244adc4e1a378c078b
RanaSumit <rana91sumit@gmail.com>
2016-01-26 14:53:02 -0800

    Ice Cream Parlor

---
+++ IceCreamParlor/Java/src/com/sumit/IceCreamParlor.java
@@ -0,0 +1,48 @@
+package com.sumit;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class IceCreamParlor
+{
+    public static void main(String[] args) throws Exception{
+
+        StringBuffer sb = new StringBuffer();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for(int T = Integer.parseInt(br.readLine()); T > 0; T--){
+            int C = Integer.parseInt(br.readLine());
+            int L = Integer.parseInt(br.readLine());
+            String[] ar = br.readLine().split(" ");
+
+            Map<Integer, List<Integer>> map = new TreeMap<Integer, List<Integer>>();
+            for(int i = 0; i < L; i++){
+                int v = Integer.parseInt(ar[i]);
+                List<Integer> list = map.get(v);
+                if (list == null){
+                    map.put(v, list = new ArrayList<Integer>());
+                }
+                list.add(i+1);
+            }
+
+            for(Integer a : map.keySet()){
+                int b = C - a;
+                a = map.get(a).remove(0);
+                List<Integer> list = map.get(b);
+                if (list != null && !list.isEmpty()){
+                    b = list.get(0);
+                    if (a > b) {
+                        int t = a;
+                        a = b;
+                        b = t;
+                    }
+                    sb.append(a + " " + b + "\n");
+                    break;
+                }
+            }
+        }
+        System.out.print(sb);
+    }
+
+}
+
