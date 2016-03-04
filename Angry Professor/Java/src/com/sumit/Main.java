f65947ae471e7fdf7509e558a89b9423f3852eb2
RanaSumit <rana91sumit@gmail.com>
2016-03-03 21:04:50 -0800

    Updated

---
+++ Angry Professor/Java/src/com/sumit/Main.java
@@ -0,0 +1,29 @@
+package com.sumit;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int a0 = 0; a0 < t; a0++) {
+            int n = in.nextInt();
+            int k = in.nextInt();
+            int a[] = new int[n];
+            for (int a_i = 0; a_i < n; a_i++) {
+                a[a_i] = in.nextInt();
+            }
+            int count = 0;
+            for(int i = 0; i < a.length; i++) {
+                if(a[i] <= 0) {
+                    count++;
+                }
+            }
+            if(count>=k)
+                System.out.println("NO");
+            else
+                System.out.println("YES");
+
+        }
+    }
+}
