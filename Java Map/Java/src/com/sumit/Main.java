4ad9f8450a7b9eb438c0ec34b3b9e2400c7f09fd
RanaSumit <rana91sumit@gmail.com>
2016-03-13 20:28:55 -0700

    Updated

---
+++ Java Map/Java/src/com/sumit/Main.java
@@ -0,0 +1,32 @@
+package com.sumit;
+
+import java.util.*;
+
+class Main{
+    public static void main(String []argh)
+    {
+        Scanner in = new Scanner(System.in);
+        HashMap phoneBook = new HashMap();
+        int n=in.nextInt();
+        in.nextLine();
+        for(int i=0;i<n;i++)
+        {
+            String name=in.nextLine();
+            int phone=in.nextInt();
+            phoneBook.put(name, phone);
+            in.nextLine();
+
+        }
+        while(in.hasNext())
+        {
+            String s=in.nextLine();
+            if(phoneBook.containsKey(s)) {
+                System.out.println(s+ "=" + phoneBook.get(s));
+            }
+            else {
+                System.out.println("Not found");
+            }
+        }
+
+    }
+}
